369aaade1cdd7fdabebe51a3f31ab3edbf805295
Garland <garland220@yahoo.com>
2017-06-24 23:41:29 -0400

    Added font-awesome, added footer, fixed flexbox issues in IE9, working on layout

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -30,11 +30,46 @@
       this.value = window.toThousands(input);
   };
 
+  window.collapseSection = function(target) {
+    var parent = target.parentElement.parentElement.parentElement;
+    var sibling = parent.nextElementSibling;
+
+    sibling.style.display = 'none';
+    target.className += ' hidden';
+    target.innerHTML = 'show <i class="fa fa-chevron-down" aria-hidden="true"></i>';
+    parent.className = '';
+  };
+
+  window.showSection = function(target) {
+    var parent = target.parentElement.parentElement.parentElement;
+    var sibling = parent.nextElementSibling;
+
+    sibling.style.display = 'block';
+    target.className = target.className.replace(' hidden', '');
+    target.innerHTML = 'hide <i class="fa fa-chevron-up" aria-hidden="true"></i>';
+    parent.className = 'title';
+  };
+
+  window.toggleSection = function(e) {
+    var target = e.target;
+
+    if (target.className.indexOf('hidden') > -1) {
+      window.showSection(target);
+    }
+    else {
+      window.collapseSection(target);
+    }
+  };
+
   window.addEventListener('load', function() {
-    var inputs = document.querySelectorAll('input.text');
+    var inputs = document.querySelectorAll('input.text'),
+        hiders = document.querySelectorAll('.toggle');
 
     for (var i=0; i<inputs.length; i+=1) {
       inputs[i].addEventListener('keyup', window.formatNumber);
+    }
+    for (var i=0; i<hiders.length; i+=1) {
+      hiders[i].addEventListener('click', window.toggleSection);
     }
 
     // flexibility(document.body);
